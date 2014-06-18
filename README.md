785f9b14fbab0e2650a67382eca29918d3800217
David Chun <daveedchun@gmail.com>
2014-06-18 17:02:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+run commands
+```
+mkdir contestapp
+cd contestapp
+rvm --ruby-version use 2.1.2@contestapp --create
+
+gem install rails -v 4.0.3 --no-ri --no-rdoc
+rails new .
+bundle install
+
+rails generate controller Dashboard index
+```
+
+add to config/routes.rb
+```
+root 'dashboard#index'
+```
+
+add to Gemfile
+```
+gem "execjs"
+gem "twitter-bootstrap-rails"
+gem "bootstrap-sass"
+```
+
+run commands
+```
+bundle install
+rails generate bootstrap:install sass
+rails g bootstrap:layout application fluid -f
+```
+
+run commands
+```
+git init
+git remote add origin path_to_remote_repository
+git add --all
+git commit -am "Initial commit"
+git push -u origin master
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contestapp::Application.config.secret_key_base = '6f2371bef814011a62a4d221b18a84beb235b1a9f82818a65c36692654530c86040974f16921c34ab1d0f560154eb3626fc21fca6834269d5eb9aabbd2fb72cc'
